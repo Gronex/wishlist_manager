16ca131b38636aecf90c90fc9ca9971b0d97fe81
Mads Slotsbo <mads.slotsbo@gmail.com>
2015-12-31 13:57:37 +0100

    playing around with infor from google

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -17,18 +17,26 @@ defmodule WishlistManager.AuthController do
     case validate_password(auth.credentials) do
       :ok ->
         # should create token here
-        user = %{id: auth.uid, name: name_from_auth(auth)} #info to give to the client
+        inspect(auth) |> IO.puts
+        user = %{id: auth.uid, name: name_from_auth(auth), provider: auth.provider, email: auth.info.email, token: auth.credentials.token} #info to give to the client
         conn
-        |> render("auth.json", data: "Success: #{inspect(user)}")
+        |> render("auth.json", data: IO.inspect(user))
       { :error, reason } ->
         conn
         |> render("auth.json", data: "Error: #{reason}")
     end
   end
 
-  defp validate_password(pass) do
-    IO.puts inspect(pass)
+  defp validate_password(credentials) do
     :ok
   end
-  defp name_from_auth(auth), do: "test"
+  defp name_from_auth(auth) do
+    if auth.info.name do
+      auth.info.name
+    else
+      name = [auth.info.first_name, auth.info.last_name]
+      |> Enum.filter(&(&1 != nil and &1 != ""))
+      if length(name) == 0, do: auth.info.nickname, else: name = Enum.join(name, " ")
+    end
+  end
 end
